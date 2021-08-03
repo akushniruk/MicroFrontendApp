b0aac69e302fb768dfc089fbb85403832e3cff6a
akushniruk <akushniruk@openware.com>
2021-08-03 09:16:38 +0300

    Feature: [vanilla JS] add microFEApp

---
+++ container/src/bootstrap.js
@@ -0,0 +1,7 @@
+import { mount as productsMount } from 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/CartShow';
+
+productsMount(document.querySelector('#dev-products'));
+cartMount(document.querySelector('#dev-cart'));
+
+console.log('Container!');
