dc2aaf0861cfab33771c53110565a94a3ec0cd64
akushniruk <akushniruk@openware.com>
2021-09-29 09:49:28 +0300

    Fix: import useHistory from react-router-dom

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,6 +1,6 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
-import { useHistory } from 'react-router';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
@@ -17,7 +17,7 @@ export default () => {
       }
     });
 
-    history.listen(onParentNavigate);
+    history?.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref} />;
